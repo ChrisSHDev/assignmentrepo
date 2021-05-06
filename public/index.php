2b4040bbb92ea08e869a6c97aa22c4a76f6abd51
ChrisSHDev <kakeochee@naver.com>
2021-05-06 16:11:12 -0700

    updated quiz 4

--- public/index.php
+++ public/index.php
@@ -31,41 +31,108 @@
   }
 ?>
 
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
   <style type="text/css">
+  body {
+    max-width: 1280px;
+    margin: 0 auto;
+  }
+
   #item-info {
     display: none;
   }
 
   .product-form-item {
     display: flex;
+    margin: 10px 0px;
   }
 
   .new-item-box {
     display: flex;
+    justify-content: center;
+    align-items: center;
   }
 
   .result-box {
     display: flex;
   }
 
-  .new-item-box {
+  .header-box {
     display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .shipmenttable {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .shipmenttable form {
+    max-width: 60%;
+  }
+
+  .shipmenttable form input {
+    width: 28px;
+    margin-right: 15px;
+    text-align: center;
+  }
+
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+  }
+
+  .productName {
+    width: 53px;
+  }
+
+  .shipmenttable form input[type="submit"] {
+    width: unset;
+    padding: 10px 20px;
+  }
+
+  button {
+    padding: 10px 20px;
+    margin: 0px 10px;
+  }
+
+  input[type="number"] {
+    width: 28px;
+  }
+
+  .new-item-box input {
+    margin-right: 15px;
+  }
+
+  .new-item-box input[type="text"] {
+    margin-right: 4px;
+  }
+
+  .header-box form input[type="submit"] {
+    padding: 10px 20px;
   }
   </style>
 
 </head>
 
 <body>
-  <h1>
-    Shipment Management System
-  </h1>
-  <button onclick="disPlayForm()">Add New Item</button>
-  <button onclick="storageReset()">Reset</button>
 
+  <div class="header-box">
+    <h1>
+      Shipment Management System
+    </h1>
+
+    <button onclick="disPlayForm()">Add New Item</button>
+    <form action="" method="post"><input type="hidden" name="reset" value=""><input type="submit" value="Reset"
+        onclick="storageReset()"></form>
+
+  </div>
   <div class="new-product">
     <div id="item-info">
       <div class="new-item-box">
@@ -89,98 +156,108 @@
     <form action="" method="post">
       <div id="product-form-box">
         <div id="new-form-box">
-          <h1>empty</h1>
         </div>
         <div class="product-form-item">
           <div class="productName">Fiddle: </div>
           <label for="productQty">Qty:</label>
-          <input type="number" name="Qty[]" value="0">
+          <input type="number" name="qty[]" value="0">
           <label for="productWeight">Weight:</label>
-          <input type="number" name="Weight[]" value="1">
+          <input type="number" name="weight[]" value="1">
 
           <div class="productDimension">Dimension:
             <label for="dimensionWidth">Width:</label>
-            <input type="number" name="Width[]" value="60">
+            <input type="number" name="width[]" value="60">
             <label for="dimensionHeight">Height:</label>
-            <input type="number" name="Height[]" value="20">
+            <input type="number" name="height[]" value="20">
             <label for="dimensionLength">Length:</label>
-            <input type="number" name="fiddleLength" value="10">
+            <input type="number" name="length[]" value="10">
           </div>
 
         </div>
         <div class="product-form-item">
           <div class="productName">Dish: </div>
           <label for="productQty">Qty:</label>
-          <input type="number" name="dishQty" value="0">
+          <input type="number" name="qty[]" value="0">
           <label for="productWeight">Weight:</label>
-          <input type="number" name="dishWeight" value="0.1">
+          <input type="number" name="weight[]" value="0.1">
 
           <div class="productDimension">Dimension:
             <label for="dimensionWidth">Width:</label>
-            <input type="number" name="dishWidth" value="30">
+            <input type="number" name="width[]" value="30">
             <label for="dimensionHeight">Height:</label>
-            <input type="number" name="dishHeight" value="30">
+            <input type="number" name="height[]" value="30">
             <label for="dimensionLength">Length:</label>
-            <input type="number" name="dishLength" value="5">
+            <input type="number" name="length[]" value="5">
           </div>
 
         </div>
         <div class="product-form-item">
           <div class="productName">Spoon: </div>
           <label for="productQty">Qty:</label>
-          <input type="number" name="spoonQty" value="0">
+          <input type="number" name="qty[]" value="0">
           <label for="productWeight">Weight:</label>
-          <input type="number" name="spoonWeight" value="0.05">
+          <input type="number" name="weight[]" value="0.05">
 
           <div class="productDimension">Dimension:
             <label for="dimensionWidth">Width:</label>
-            <input type="number" name="spoonWidth" value="15">
+            <input type="number" name="width[]" value="15">
             <label for="dimensionHeight">Height:</label>
-            <input type="number" name="spoonHeight" value="5">
+            <input type="number" name="height[]" value="5">
             <label for="dimensionLength">Length:</label>
-            <input type="number" name="spoonLength" value="2">
+            <input type="number" name="length[]" value="2">
           </div>
 
         </div>
 
       </div>
+      <input type="hidden" name="result" value="result">
       <input type="submit" name="submit" value="Submit">
     </form>
-  </div>
-  <div class="shipment-result">
-    <?php
+    <div class="shipment-result">
+      <?php
+     if (isset($_POST['result'])) {
+         $product = new Products;
+
+         for ($i=0; $i < count($_POST['weight']); $i++) {
+             $product -> set_productWeights($_POST['qty'][$i], $_POST['weight'][$i]);
+             $product -> set_productDimensions($_POST['qty'][$i], ($_POST['width'][$i] * $_POST['height'][$i]*$_POST['length'][$i]));
+         }
+     }
      
-      if (isset($_POST['fiddleQty'])) {
-          $product = new Products;
-        
-          $product -> set_productWeights($_POST['fiddleQty'], $_POST['fiddleWeight']);
-          $product -> set_productWeights($_POST['dishQty'], $_POST['dishWeight']);
-          $product -> set_productWeights($_POST['spoonQty'], $_POST['spoonWeight']);
+   ?>
+      <div class="result">
 
-          $product -> set_productDimensions($_POST['fiddleQty'], ($_POST['fiddleWidth'] * $_POST['fiddleHeight']*$_POST['fiddleLength']));
-          $product -> set_productDimensions($_POST['dishQty'], ($_POST['dishWidth'] * $_POST['dishHeight']*$_POST['dishLength']));
-          $product -> set_productDimensions($_POST['spoonQty'], ($_POST['spoonWidth'] * $_POST['spoonHeight']*$_POST['spoonLength']));
-      }
-    ?>
-    <div class="result">
-      <?php if (isset($_POST['fiddleQty'])): ?>
-      <div class="result-box">
-        <p>Total Weights: </p>
-        <p><?php echo htmlspecialchars((array_sum($product -> get_productWeights())), ENT_QUOTES, 'UTF-8'); ?>kg</p>
+        <?php if (!empty($_POST['result'])): ?>
+        <div class="result-wrap">
+          <div class="result-box">
+            <p>Total Weights: </p>
+            <p><?php echo htmlspecialchars((array_sum($product -> get_productWeights())), ENT_QUOTES, 'UTF-8'); ?>kg
+            </p>
+          </div>
+          <div class="result-box">
+            <p>Total Dimensions: </p>
+            <p>
+              <?php echo htmlspecialchars((array_sum($product -> get_productDimensions())), ENT_QUOTES, 'UTF-8'); ?>cm
+            </p>
+          </div>
+        </div>
+
+        <?php endif ?>
       </div>
-      <div class="result-box">
-        <p>Total Dimensions: </p>
-        <p><?php echo htmlspecialchars((array_sum($product -> get_productDimensions())), ENT_QUOTES, 'UTF-8'); ?>cm</p>
-      </div>
-      <?php endif ?>
+
     </div>
-
   </div>
+
   <script>
   let newFormBox = document.getElementById('new-form-box');
   console.log(newFormBox);
 
   function storageReset() {
+    let result = document.getElementsByClassName('result')[0];
+    let childResult = document.getElementsByClassName('result-wrap')[0];
+    console.log(result);
+    console.log(childResult);
+    result.removeChild(childResult);
     localStorage.clear();
     location.reload();
   }
@@ -203,20 +280,19 @@
 
     div.innerHTML += `
     <div class="product-form-item">
-          <div class="productName">` + newProductName + ` </div>
-          <label for="productQty">: Qty:</label>
-          <input type="number" name="` + newProductName + `Qty" value="0">
+          <div class="productName">` + newProductName + ` :</div>
+          <label for="productQty">Qty:</label>
+          <input type="number" name="qty[]" value="0">
           <label for="productWeight">Weight:</label>
-          <input type="number" name="` + newProductName + `Weight" value="` + newProductWeight + `">
+          <input type="number" name="weight[]" value="` + newProductWeight + `">
 
           <div class="productDimension">Dimension:
             <label for="dimensionWidth">Width:</label>
-            <input type="number" name="` + newProductName + `Width" value="` +
-      newWidth + `">
+            <input type="number" name="width[]" value="` + newWidth + `">
             <label for="dimensionHeight">Height:</label>
-            <input type="number" name="` + newProductName + `Height" value="` + newHeight + `">
+            <input type="number" name="height[]" value="` + newHeight + `">
             <label for="dimensionLength">Length:</label>
-            <input type="number" name="` + newProductName + `Length" value="` + newLength + `">
+            <input type="number" name="length[]" value="` + newLength + `">
           </div>
 
         </div>
